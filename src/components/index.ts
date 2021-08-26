a2377b5b5c8ea298438b0a8e63d923e06a6e6717
Caique Lourenço <caique.lourenco@modalgr.com.br>
2021-08-26 19:00:16 -0300

    Criou arquivo principal de components

---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import Menu from './Menu';
+import Row from './Row';
+import H1 from './H1';
+import Hr from './Hr';
+import Select from './Select';
+import Input from './Input';
+import Drivers from './Drivers';
+import BtnCheck from './CheckBox';
+import Button from './Button';
+import Card from './Card';
+import Column from './Column';
+import BtnBack from './BtnBack';
+import Grid from './Grid';
+
+export {
+  Menu,
+  Card,
+  Row,
+  H1,
+  Hr,
+  Button,
+  Select,
+  Input,
+  Drivers,
+  BtnCheck,
+  Column,
+  BtnBack,
+  Grid,
+};
